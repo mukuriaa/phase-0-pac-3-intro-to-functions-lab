7209679ae45f5a0510ccef025654a1b1ba6671c2
Eric Mukuria <mukuriaeric@gmail.com>
2022-06-04 08:02:29 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,46 @@
+function shout(string) {
+    // todo
+  }
+  function shout(string) {
+    return string;
+  }
+  "Hello!".toUpperCase(); // 'HELLO!'
+  function shout(string) {
+    return string.toUpperCase();
+  }
+ function whisper(string) {
+     // todo
+ }
+ function whisper(string) {
+     return string;
+ }
+ "Hello!".toLowerCase(); // 'hello!'
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  function logShout(string) {
+    console.log(string.toUppercase)}
+
+    function logShout(str){
+        console.log(str.toUpperCase())
+    }
+
+    function logWhisper(str){
+        console.log(str.toLowerCase())
+    }
+
+    function sayHiToHeadphonedRoommate(string){
+        var look="I can\'t hear you!";
+        var quick="YES INDEED!";
+        var step="I would love to!";
+        if (string.toLowerCase(string)===string){
+            return look;
+        }else if(string.toUpperCase(string)===string){
+            return quick;
+        }else if("Let's have dinner together!"===string){
+            return step;
+        }
+    }
+
+
+    
